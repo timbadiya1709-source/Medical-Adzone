f108b2bbda49dec99979bfaf7da087e353978f66
Vivek Timbadiya <timbadiya1709@gmail.com>
2026-01-16 16:31:58 +0530

    Player Movement

--- Assets/Script/Player Movement.cs
+++ Assets/Script/Player Movement.cs
@@ -5,36 +5,47 @@ public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 150f;
     [SerializeField] InputAction horizontal;
-    [SerializeField] InputAction vertical;  
+    [SerializeField] InputAction vertical;
+    [SerializeField] InputAction mouseinput;
+    float yaw = 0f;
     Rigidbody rb;
     void OnEnable()
     {
         horizontal.Enable();
         vertical.Enable();
+        mouseinput.Enable();
     }
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        yaw = transform.eulerAngles.y;
     }
 
 
-    void FixedUpdate()
+    public void FixedUpdate()
     {
-        if (horizontal.IsPressed())
-        {
-            rb.linearVelocity = new Vector3(horizontal.ReadValue<float>(), 0, 0) * Speed * Time.deltaTime;
-        }
-        if (vertical.IsPressed())
-        {
-            rb.linearVelocity = new Vector3(0, 0, vertical.ReadValue<float>()* Speed * Time.deltaTime);
-        }
-        if (horizontal.IsPressed() && vertical.IsPressed())
-        {
-            rb.linearVelocity = new Vector3(horizontal.ReadValue<float>(), 0, vertical.ReadValue<float>()) * Speed * Time.deltaTime;
-        }
-            else if (!horizontal.IsPressed() && !vertical.IsPressed())
-        {
-            rb.linearVelocity = Vector3.zero;
-        }
+        playermovement();
+        playerotation();
+    }
+
+    private void playermovement()
+    {
+        float h = horizontal.IsPressed() ? horizontal.ReadValue<float>() : 0f;
+        float v = vertical.IsPressed() ? vertical.ReadValue<float>() : 0f;
+        Vector3 localMove = new Vector3(h, 0f, v).normalized * Speed * Time.deltaTime;
+        Vector3 worldMove = transform.TransformDirection(localMove);
+        worldMove.y = rb.linearVelocity.y;
+        rb.linearVelocity = worldMove;
+    }
+
+
+    private void playerotation()
+    {
+        Vector2 mouseinputvector = mouseinput.ReadValue<Vector2>();
+        yaw += mouseinputvector.x;
+        Quaternion targetrotation = Quaternion.Euler(0, yaw * Time.deltaTime, 0);
+        rb.MoveRotation(targetrotation);
     }
 }
